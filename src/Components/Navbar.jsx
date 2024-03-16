4e432208b0b813bbeaebc1695779edda36826e3b
Akash M Nandan <akash003nandan@gmail.com>
2024-03-17 01:09:23 +0530

    navbar html

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Logo from '../assets/Ep-Hub.png'
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <img src="" alt="" />
+        <img src={Logo} alt="" />
+      </div>
+      <div className="menu">
+        <li>
+          <ul>Home</ul>
+          <ul>Shop</ul>
+          <ul>My Orders</ul>
+          <div className="login-register">
+            <ul>Login</ul>
+            <ul>Register</ul>
+          </div>
+        </li>
       </div>
     </nav>
   )
