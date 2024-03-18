fee3d535ca07426c6233238d6595d770a002ab88
Akash M Nandan <akash003nandan@gmail.com>
2024-03-18 11:18:09 +0530

    about us set

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -5,10 +5,12 @@ const AboutUs = () => {
   return (
     <div className="about-us">
        <div className="content">
-            <h4>Welcome to EP-HUB!</h4>
-            <p>your comprehensive solution for efficient and sustainable management of plastic waste. At EP-Hub, we are dedicated to revolutionizing the way plastic waste is managed, mitigating its environmental impact, and promoting a circular economy. Our platform offers a user-friendly interface designed to streamline the process of plastic waste management for individuals, businesses, and organizations alike. Whether you are a small-scale recycler, a large corporation, or a concerned citizen looking to make a difference, eP-Hub provides the tools and resources you need to take meaningful action against plastic pollution.</p>
+            <h4>Welcome to EP-HUB!</h4> <br /><br />
+            <p>At EP-Hub, we are dedicated to revolutionizing the way plastic waste is managed, mitigating its environmental impact, and promoting a circular economy.<br/><br/><br/> Our platform offers a user-friendly interface designed to streamline the process of plastic waste management for individuals, businesses, and organizations alike. Whether you are a small-scale recycler, a large corporation, or a concerned citizen looking to make a difference, EP-Hub provides the tools and resources you need to take meaningful action against plastic pollution.</p>
        </div>
-       <img src={image} alt="" className="image" />
+       <div className="image-container">
+        <img src={image} alt="" className='image'/>
+       </div>
     </div>
   )
 }
