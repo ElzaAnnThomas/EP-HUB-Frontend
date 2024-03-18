86eac80e022f338e2e9b7e097c3b9684deaa6b83
Akash M Nandan <akash003nandan@gmail.com>
2024-03-18 15:31:53 +0530

    swiper import

--- src/Components/Roles.jsx
+++ src/Components/Roles.jsx
@@ -1,3 +1,16 @@
+import { useEffect, useState } from 'react'
+import demo1 from '../assets/job-seeker.jpg'
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 
 const Roles = () => {
