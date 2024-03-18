52a1c690a6a1478a20484f25fe393fe90cd93fd6
Akash M Nandan <akash003nandan@gmail.com>
2024-03-18 15:34:29 +0530

    swiper html set

--- src/Components/Roles.jsx
+++ src/Components/Roles.jsx
@@ -15,7 +15,71 @@ import 'swiper/css/scrollbar';
 
 const Roles = () => {
   return (
-    <div>Roles</div>
+    <section className="h-screen w-full flex flex-col items-center mt-20">
+        <div className="text-ttOrange uppercase font-semibold tracking-widest">Programs</div>
+        <div className="space-grotesk text-[45px] w-2/3 text-center font-bold mt-4 max-sm:text-3xl max-md:text-3xl max-lg:text-3xl">Are you budding founder? We have the perfect program for you.</div>
+
+        <div className='flex flex-row h-92 mt-20 w-2/3 justify-center  overflow-hidden'>
+
+
+        <Swiper
+      // install Swiper modules
+    
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      slidesPerView={noSlides}
+      //navigation
+      spaceBetween={10}
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+     className='flex items-center max-sm:ml-16 max-md:ml-32'
+    >
+      <SwiperSlide><div className='flex flex-col w-56 h-full pb-5 justify-center '>
+              <img className='h-48 rounded-t-lg' src={demo1} alt="image" />
+              <div className='flex  flex-col w-56 p-3'>
+                <p className='text-md font-bold'>Course</p>
+                <p className='text-xs font-light'>Course details, mentors, and others </p>
+              </div>
+            </div></SwiperSlide>
+      <SwiperSlide><div className=' flex flex-col w-56 h-full '>
+              <img className='h-48 rounded-t-lg' src={demo1} alt="image" />
+              <div className='flex  flex-col w-full p-3'>
+                <p className='text-md font-bold'>Course</p>
+                <p className='text-xs font-light'>Course details, mentors, and others </p>
+              </div>
+            </div></SwiperSlide>
+      <SwiperSlide><div className=' flex flex-col w-56 h-full'>
+              <img className='h-48 rounded-t-lg' src={demo1} alt="image" />
+              <div className='flex  flex-col w-full p-3'>
+                <p className='text-md font-bold'>Course</p>
+                <p className='text-xs font-light'>Course details, mentors, and others </p>
+              </div>
+            </div></SwiperSlide>
+      <SwiperSlide><div className=' flex flex-col w-56 h-full '>
+              <img className='h-48 rounded-t-lg' src={demo1} alt="image" />
+              <div className='flex  flex-col w-full p-3'>
+                <p className='text-md font-bold'>Course</p>
+                <p className='text-xs font-light'>Course details, mentors, and others </p>
+              </div>
+            </div></SwiperSlide>
+            <SwiperSlide><div className=' flex flex-col w-56 h-full'>
+              <img className='h-48 rounded-t-lg' src={demo1} alt="image" />
+              <div className='flex  flex-col w-full p-3'>
+                <p className='text-md font-bold'>Course</p>
+                <p className='text-xs font-light'>Course details, mentors, and others </p>
+              </div>
+            </div></SwiperSlide>
+            <SwiperSlide><div className=' flex flex-col w-56 h-full'>
+              <img className='h-48 rounded-t-lg' src={demo1} alt="image" />
+              <div className='flex  flex-col w-full p-3'>
+                <p className='text-md font-bold'>Course</p>
+                <p className='text-xs font-light'>Course details, mentors, and others </p>
+              </div>
+            </div></SwiperSlide>
+    </Swiper>
+    </div>
+
+      </section>
   )
 }
 
