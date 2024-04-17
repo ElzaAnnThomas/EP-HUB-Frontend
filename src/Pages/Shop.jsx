de9b338d6e086ed3ac4b281aa3561d62e82cd3aa
Akash M Nandan <akash003nandan@gmail.com>
2024-04-17 23:01:47 +0530

    shop page html set

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,4 +1,8 @@
 import Navbar from "../Components/Navbar"
+import sellerdesc from "../assets/seller-desc.png"
+import buyerdesc from "../assets/buyer-desc.png"
+import how from "../assets/how-it-works.png"
+
 
 
 const Shop = () => {
@@ -10,7 +14,16 @@ const Shop = () => {
 
       {/* shoping page options */}
 
-      
+      <div>
+        <img src={how} alt="" />
+        <div>
+          <button>Seller</button>
+          <button>Buyer</button>
+        </div>
+        <img src={sellerdesc} alt="" />
+        <img src={buyerdesc} alt="" />
+      </div>
+
       
 
     </div>
