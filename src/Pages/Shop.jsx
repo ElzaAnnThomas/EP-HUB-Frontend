1abf6799f22b765504f94b69c89e12bd3f41c15b
Akash M Nandan <akash003nandan@gmail.com>
2024-04-17 23:12:46 +0530

    buyer seller toggle set

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,33 +1,42 @@
-import Navbar from "../Components/Navbar"
-import sellerdesc from "../assets/seller-desc.png"
-import buyerdesc from "../assets/buyer-desc.png"
-import how from "../assets/how-it-works.png"
-
-
+import Navbar from "../Components/Navbar";
+import sellerdesc from "../assets/seller-desc.png";
+import buyerdesc from "../assets/buyer-desc.png";
+import how from "../assets/how-it-works.png";
+import { useState } from "react";
 
 const Shop = () => {
+  const [sellerClicked, setSellerClicked] = useState(true);
+  const [buyerClicked, setBuyerClicked] = useState(false);
+
+  const handleSellerClick = () => {
+    setSellerClicked(true);
+    setBuyerClicked(false);
+  };
+
+  const handleBuyerClick = () => {
+    setBuyerClicked(true);
+    setSellerClicked(false);
+  };
+
   return (
     <div>
       {/* importing Navbar */}
-
-      <Navbar/>
+      <Navbar />
 
       {/* shoping page options */}
-
       <div>
         <img src={how} alt="" />
         <div>
-          <button>Seller</button>
-          <button>Buyer</button>
+          <button onClick={handleSellerClick}>Seller</button>
+          <button onClick={handleBuyerClick}>Buyer</button>
         </div>
-        <img src={sellerdesc} alt="" />
-        <img src={buyerdesc} alt="" />
+        
+        {/* Conditional rendering based on button clicks */}
+        {sellerClicked && <img src={sellerdesc} alt="" />}
+        {buyerClicked && <img src={buyerdesc} alt="" />}
       </div>
-
-      
-
     </div>
-  )
-}
+  );
+};
 
-export default Shop
+export default Shop;
