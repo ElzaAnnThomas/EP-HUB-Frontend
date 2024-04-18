64347ffa827c284643ee0b236bedcecd30bd8b3c
Akash M Nandan <akash003nandan@gmail.com>
2024-04-18 08:11:34 +0530

    active class on navbar set

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,79 @@
-import Logo from '../assets/Ep-Hub.png' 
-import menuButton from '../assets/menu-button.png'
-import { Link } from 'react-router-dom'
+import Logo from '../assets/Ep-Hub.png';
+import menuButton from '../assets/menu-button.png';
+import { Link, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(location.pathname);
+
+  const handleSetActiveLink = (path) => {
+    setActiveLink(path);
+  };
+
   return (
-    <nav className='flex flex-row items-center justify-between bg-[#92E3A9] h-20 rounded-b-2xl'>
-      <img src={Logo} alt="ep-hub"  className='h-5 w-28 ml-5'/>
-      <div className="max-sm:hidden flex flex-row mr-12 gap-10">
-        <Link to='/'>Home</Link>
-        <Link to='/shop'>Shop</Link>
-        <Link to='/my-orders'>My Orders</Link>
+    <nav className='flex flex-row items-center justify-between bg-[#92E3A9] h-20 rounded-b-2xl px-5 sm:px-10'>
+      {/* Logo */}
+      <img src={Logo} alt="ep-hub" className='h-5 w-28' />
+
+      {/* Navigation Links - Desktop */}
+      <div className="hidden sm:flex flex-row gap-10">
+        <Link 
+          to='/' 
+          onClick={() => handleSetActiveLink('/')}
+          className={`
+            ${activeLink === '/' ? 'text-white font-semibold' : 'text-gray-600'}
+          `}
+        >
+          Home
+        </Link>
+        <Link 
+          to='/shop' 
+          onClick={() => handleSetActiveLink('/shop')}
+          className={`
+            ${activeLink === '/shop' ? 'text-white font-semibold' : 'text-gray-600'}
+          `}
+        >
+          Shop
+        </Link>
+        <Link 
+          to='/my-orders' 
+          onClick={() => handleSetActiveLink('/my-orders')}
+          className={`
+            ${activeLink === '/my-orders' ? 'text-white font-semibold' : 'text-gray-600'}
+          `}
+        >
+          My Orders
+        </Link>
         <div className="flex flex-row gap-1">
-          <Link to='/login'>Login</Link>
+          <Link 
+            to='/login' 
+            onClick={() => handleSetActiveLink('/login')}
+            className={`
+              ${activeLink === '/login' ? 'text-white font-semibold' : 'text-gray-600'}
+            `}
+          >
+            Login
+          </Link>
           <p>|</p>
-          <Link to='/register'>Register</Link>
+          <Link 
+            to='/register' 
+            onClick={() => handleSetActiveLink('/register')}
+            className={`
+              ${activeLink === '/register' ? 'text-white font-semibold' : 'text-gray-600'}
+            `}
+          >
+            Register
+          </Link>
         </div>
       </div>
+
+      {/* Menu Button - Mobile */}
       <button className='sm:hidden'>
-        <img src={menuButton} alt="" className='h-20' />
+        <img src={menuButton} alt="Menu" className='h-20' />
       </button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
