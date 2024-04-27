8e0cc165699322dbce281ba93bceeff091d41d42
Akash M Nandan <akash003nandan@gmail.com>
2024-04-27 15:04:27 +0530

    carosuel structure made

--- src/Components/OfferCarousel.jsx
+++ src/Components/OfferCarousel.jsx
@@ -2,11 +2,20 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 const OfferCarousel = () => {
+  
+
   return (
     <div>
-        
+      <Carousel autoPlay infiniteLoop>
+        {offers.map(offer => (
+          <div key={offer.id}>
+            <img src={offer.image} alt={offer.title} />
+            <p className="legend">{offer.title}</p>
+          </div>
+        ))}
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default OfferCarousel
+export default OfferCarousel;
