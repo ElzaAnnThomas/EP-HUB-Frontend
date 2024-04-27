83ca2ea8f41c862f367b56dbcdf9767fc2e33047
Akash M Nandan <akash003nandan@gmail.com>
2024-04-27 15:08:49 +0530

    carousel data set created

--- src/Components/OfferCarousel.jsx
+++ src/Components/OfferCarousel.jsx
@@ -1,8 +1,23 @@
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
+//offers images import here
+import offer1 from '../assets/offer1.jpg';
+import offer2 from '../assets/offer2.jpeg';
+import offer3 from '../assets/offer3.jpeg';
+import offer4 from '../assets/offer4.jpg';
+import offer5 from '../assets/offer5.webp';
+
+
+
 const OfferCarousel = () => {
-  
+    const offers = [
+        { id: 1, image: offer1, title: 'Offer 1' },
+        { id: 2, image: offer2, title: 'Offer 2' },
+        { id: 3, image: offer3, title: 'Offer 3' },
+        { id: 1, image: offer4, title: 'Offer 1' },
+        { id: 2, image: offer5, title: 'Offer 2' },
+      ];
 
   return (
     <div>
